310eb4a40e31f4eb493cbdcbae28313c3f1d8cf6
mhasley-school <mhasley@charlotte.edu>
2026-07-27 14:37:09 -0500

    Delete FinalProject/FinalProject directory

--- FinalProject/FinalProject/api/routers/index.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from api.routers import orders, menu_items
-
-
-def load_routes(app: FastAPI):
-    app.include_router(menu_items.router)
-    app.include_router(orders.router)
